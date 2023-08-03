d9e4be7b9d1cf408fc01929daac355ef477e105f
nessasaddi <134745638+nessasaddi@users.noreply.github.com>
2023-08-02 22:04:00 -0700

    Cleaned up code

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+// Function to get the current date and time
 function getCurrentDateAndTime() {
   // Your existing function here
 }
 
-// Set the current date and time on page load
-document.addEventListener("DOMContentLoaded", function () {
-  const descElement = document.querySelector(".desc");
-  descElement.textContent = getCurrentDateAndTime();
-});
-
+// Function to search the weather of a city
 function searchCity(event) {
-  event.preventDefault(); // Prevent form submission's default behavior (page reload)
+  // Prevent form submission's default behavior (page reload)
+  event.preventDefault();
 
+  // Get city name from the search input
   const searchInput = document.getElementById("searchInput");
   const cityName = searchInput.value;
 
@@ -18,16 +16,25 @@ function searchCity(event) {
   const apiKey = "bf54175800a55e59e6c4d6461deeef12";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
+  // Fetch weather data from the OpenWeatherMap API
   axios
     .get(apiUrl)
     .then((response) => {
+      // Set the current temperature on the page
       const currentTemperature = response.data.main.temp;
       const h1Element = document.getElementById("currentTemperature");
       h1Element.textContent = `Current Temperature: ${currentTemperature} °C`;
     })
     .catch((error) => {
+      // Log error and set the error message on the page
       console.error("Error fetching weather data:", error);
       const h1Element = document.getElementById("currentTemperature");
       h1Element.textContent = "City not found";
     });
 }
+
+// Set the current date and time on page load
+document.addEventListener("DOMContentLoaded", function () {
+  const descElement = document.querySelector(".desc");
+  descElement.textContent = getCurrentDateAndTime();
+});
