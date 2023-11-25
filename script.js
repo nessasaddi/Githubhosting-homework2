bc75443f00caeb02e109fd9ef1d4b2add09a462e
nessasaddi <134745638+nessasaddi@users.noreply.github.com>
2023-11-24 23:04:46 -0800

    duplicate

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+  const date = new Date(timestamp);
+  let hours = date.getHours().toString().padStart(2, '0');
+  let minutes = date.getMinutes().toString().padStart(2, '0');
 
-  let days = [
+  const days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -18,21 +12,22 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDay()];
+  const day = days[date.getDay()];
+
   return `${day} ${hours}:${minutes}`;
 }
 
-let temperatureElement = document.querySelector(".weatherbox h2");
+const temperatureElement = document.querySelector(".weatherbox h1");
 let celsiusTemperature = null;
 
 function displayTemperature(response) {
   celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  let cityElement = document.querySelector(".nav-link");
-  let informationElement = document.querySelector(".information");
-  let dateElement = document.querySelector(".weatherbox h3");
-  let iconElement = document.querySelector("#icon");
+  const cityElement = document.querySelector(".nav-link");
+  const informationElement = document.querySelector(".information");
+  const dateElement = document.querySelector(".weatherbox h2");
+  const iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.name;
   informationElement.innerHTML = `Weather: ${
@@ -42,15 +37,19 @@ function displayTemperature(response) {
   }%`;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
-  let iconCode = response.data.weather[0].icon;
-  let iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  const iconCode = response.data.weather[0].icon;
+  const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
   iconElement.setAttribute("src", iconUrl);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function convertCelsiusToFahrenheit(celsiusTemperature) {
+  return (celsiusTemperature * 9) / 5 + 32;
+}
+
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  const fahrenheitTemperature = convertCelsiusToFahrenheit(celsiusTemperature);
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
@@ -59,21 +58,15 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
 function searchCity(city) {
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#searchInput");
+  const cityInputElement = document.querySelector("#searchInput");
   searchCity(cityInputElement.value);
 }
 
@@ -82,10 +75,10 @@ function handleCityClick(event) {
   searchCity(event.target.textContent);
 }
 
-let form = document.querySelector("form");
+const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
-let cityLinks = document.querySelectorAll(".nav-link");
+const cityLinks = document.querySelectorAll(".nav-link");
 cityLinks.forEach((link) => {
   link.addEventListener("click", handleCityClick);
 });
